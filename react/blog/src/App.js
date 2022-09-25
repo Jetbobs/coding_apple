cc93e0e70d995be8d1ac6311af0c6b8b377980da
JBY <qpv008@gmail.com>
2022-09-26 03:57:58 +0900

    0926

---
+++ react/blog/src/App.js
@@ -0,0 +1,137 @@
+
+
+import logo from './logo.svg';
+import './App.scss';
+import React, {useState} from'react';
+
+function App() {
+
+  let post = '강남 우동 맛집';
+  let [post1,p1] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
+  let [like,l1] = useState([0,0,0]);
+  let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [inputs,setInputs] = useState('');
+
+  [1,2,3].map(function(){
+
+  })
+
+   function likes(){
+    l1(like + 1);
+   }
+   function likesArr(){
+
+
+   }
+   function chP(){
+    let pArray = [...post1];
+    pArray[0] = '여자코트 추천';
+    p1(pArray);
+   }
+   function ganada(a,b){
+    if(a<b){
+      return -1;
+    } else if(a>b){
+      return 1;
+    }
+  }
+   function sortArray(){
+    let sArray = [...post1];
+    sArray.sort(ganada)
+      p1(sArray);
+    }
+
+    let count = 1;
+    function modalPop(){
+      count++;
+      if(count%2 == 0){
+        setModal(true);
+      } else if (count%2 == 1){
+        setModal(false);
+      }
+    }
+
+  
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      {/* <div className="list">
+        <h4>{post1[0]} <span style={{cursor:'pointer'}} onClick={likes}>👍</span> {like} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{post1[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4 onClick={()=>{ setModal(!modal) }}>{post1[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div> */}
+      {
+        post1.map(function(a,b){
+          return(
+            <div className="list" key={b}>
+            <h4 onClick={()=>{ setModal(!modal); setTitle(b)}}>{post1[b]} <span style={{cursor:'pointer'}} onClick={(e) => {e.stopPropagation();
+              let lArr = [...like];
+              lArr[b] = lArr[b] + 1;
+              l1(lArr);
+            }}>👍</span> {like[b]} </h4>
+            <p>2월 17일 발행</p> <button onClick={()=>{
+                  let newPost2 = [...post1]; //현재 게시물을 newPost2에 할당
+                  newPost2.splice(b,1);
+                  p1(newPost2);
+            }}>삭제</button>
+          </div>
+          )
+        })
+      }
+      <input onChange={(e)=>{setInputs(e.target.value); console.log(e.target.value)}} type="text"></input>
+      <button onClick={()=>{
+            let newPost = [...post1];
+            newPost.push(inputs);
+            p1(newPost);
+      }}>글발행</button>
+            {
+        modal == true ? <Modal post1={post1} revise={chP} title={title}/> : null
+      }
+    </div>
+  );
+}
+
+//component
+function Modal(props){
+  return(
+    <div className="modal">
+    <h4>{props.post1[props.title]}</h4>
+    <p>날짜</p>
+    <p>상세내용</p>
+    <button onClick={props.revise}>글수정</button>
+  </div>
+  )
+};
+
+class Modal2 extends React.Component{
+  constructor(props){
+    super(props);
+    this.state= {
+      name: 'kim',
+      age: 20
+    }
+  }
+  render(){
+    return(
+      <div>안녕 {this.state.name}
+              <button onClick={()=> {
+        this.setState({age: 21})
+      }}>버튼</button>
+      </div>
+  
+    )
+  }
+}
+
+export default App;
---
+++ react/shop/src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+import {Button,Navbar,Container,Nav} from 'react-bootstrap';
+import { useState } from 'react';
+import shoesData from './js/data';
+import {Route, Routes, Link} from 'react-router-dom'
+// import imgBg from './img/bg.png';
+
+function App() {
+
+  let [shoes] = useState(shoesData);
+  console.log(shoes.length)
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/detail' element={<div>상세페이지임</div>}></Route>
+        <Route path='/about' element={<div>어바웃페이지임</div>}></Route>
+      </Routes>
+        <Navbar bg="blue" variant="dark" className='navbar'>
+        <Container className='container'>
+          <Navbar.Brand href="#home" className='navbar-brand'>Natti's Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          {/* components만 사용했을 때 */}
+          {/* <Products shoes={shoes} i={0}/>
+          <Products shoes={shoes} i={1}/>
+          <Products shoes={shoes} i={2}/> */}
+          {
+            //map 과 components를 사용했을 때
+            shoes.map(function(a,b){
+              return(
+                <Products shoes={shoes[b]} i={b}/>
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+// components
+function Products(props){
+  return(
+    <div className="col-md-4">
+    <img src={"https://codingapple1.github.io/shop/shoes"+(props.i + 1)+".jpg"} width={'80%'} alt="" />
+    <h4>{props.shoes.title}</h4>
+    <p>{props.shoes.content}</p>
+    <p>{props.shoes.price}원</p>
+  </div>
+  )
+}
+
+export default App;
