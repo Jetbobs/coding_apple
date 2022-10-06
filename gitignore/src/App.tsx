70f32306160f1eda7cbb1f20c6b3aba32d4abbee
JBY <qpv008@gmail.com>
2022-10-06 18:41:27 +0900

    1006

--- gitignore/src/App.tsx
+++ gitignore/src/App.tsx
@@ -10,7 +10,7 @@ import { Nav, Button, Container, Form, Navbar, NavDropdown } from 'react-bootstr
 import TitleLogo from './img/gitignore_logo.png'
 //fontawesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 
 function App() {
@@ -48,8 +48,79 @@ function App() {
         </Navbar>
       </div>
       <div id="content-container">
-        <div id="sidebar"></div>
-        <div id="main-content"></div>
+        <div id="sidebar">
+          <nav className="sidebar-menu-container">
+            <ul className="sidebar-menu">
+              <a href=""><li className="sidebar-menu-list">
+                gitignore.io
+              </li></a>
+            </ul>
+            <ul className="sidebar-menu menu-margin" >
+              <li className="sidebar-title">
+                INSTALL
+              </li>
+              <ul className="sidebar-menu-list-container">
+                <a href=""><li className="sidebar-menu-list">
+                  Command Line
+                </li></a>
+                <a href=""><li className="sidebar-menu-list">
+                  Editor Extensions
+                </li></a>
+                <a href=""><li className="sidebar-menu-list">
+                  Client Applications
+                </li></a>
+                <a href=""><li className="sidebar-menu-list">
+                  Local Server
+                </li></a>
+              </ul>
+            </ul>
+          </nav>
+        </div>
+        <div id="main-content">
+          <div className="main">
+            <div className="main-title">
+              <h1>
+                Command Line
+              </h1>
+              <p className="main-title-sub">
+                To run gitignore.io from your command line you need an active internet connection and an environment function. You need to add a function to your environment that lets you access the gitignore.io API.
+              </p>
+            </div>
+            <div className="contents-container">
+              <div className="contents-item">
+                <h2 className='content-title'>Git</h2>
+                <p className="content-subtitle">Bash</p>
+              </div>
+              <div className="prev-next-btn-container ta-r">
+                <div className="prev-next-btn">
+                  <FontAwesomeIcon className='arrow-icon' icon={faArrowLeft} />
+                  <div className="btn-text-container">
+                    <div className="btn-title">
+                      Previous
+                    </div>
+                    <div className="btn-dir">
+                      <span>gitignore.io</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="prev-next-btn ta-l">
+                  <div className="btn-text-container">
+                    <div className="btn-title">
+                      <p>Next</p>
+                    </div>
+                    <div className="btn-dir">
+                      <span>gitignore.io</span>
+                    </div>
+                  </div>
+                  <FontAwesomeIcon className='arrow-icon' icon={faArrowRight} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="side-nav">
+            <nav></nav>
+          </div>
+        </div>
       </div>
     </div>
   );
