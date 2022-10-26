6eae0a79acf835ca7a87d0f2fc0c95f7dd9140d4
JBY <qpv008@gmail.com>
2022-10-26 18:27:26 +0900

    221026 리액트 복습

--- node/server.js
+++ node/server.js
@@ -2,14 +2,30 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
 
 
 app.use(bodyParser.urlencoded({extended : true}));
+
+let db;
 MongoClient.connect('mongodb+srv://admin_jb:doslzja15@cluster0.e1hz11b.mongodb.net/?retryWrites=true&w=majority',function(err, client){
+   
+    db = client.db('todoapp');
+
+    
+
     app.listen(8080, function(){
         console.log('listening on 8080')
     });
 });
+
+app.post('/add',function(req,res){
+    console.log('add 됨')
+    db.collection('post').insertOne({이메일 : req.body.email, 텍스트 : req.body.txtarea}, function(err, result){
+        console.log('SAVE');
+    })
+});
+
 
 app.get('/pet',function(req, res){
     res.send('펫 용품을 쇼핑할 수 있는 페이지입니다.')
@@ -22,8 +38,7 @@ app.get('/',function(req,res){
 })
 app.get('/write',function(req,res){
     res.sendFile(__dirname + '/write.html')
-})
-app.post('/add',function(req,res){
-    res.send('전송완료')
-    console.log(req.body.txtarea)
+});
+app.get('/list',function(req,res){
+    res.render('list.ejs');
 })
--- study-react/src/App.js
+++ study-react/src/App.js
@@ -1,18 +1,44 @@
-import logo from './logo.svg';
+
+
 import './App.scss';
+import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집';
+  let [post,postSet] = useState(['남자 코트 추천','강남 우동맛집','파이썬독학']);
+  let [thumb, thumbSet] = useState(0);
+
+  function titleChange(){
+    let postCopy = [...post];
+     postCopy[0] = '여자 코트 추천';
+     postSet(postCopy);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>블로그임</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{post[0]}</h4>
+        <p>2월 17일 발행 <span onClick={function(){
+          thumbSet(thumb + 1)
+        }}>❤</span> {thumb}</p>
+        <button onClick={titleChange}>버튼</button>
+      </div>
+        <Modal></Modal>
     </div>
   );
+}
+
+function Modal(){
+  return(
+    <div className="modal">
+    <h4>제목</h4>
+    <p>날짜</p>
+    <p>상세내용</p>
+  </div>
+  )
 }
 
 export default App;
