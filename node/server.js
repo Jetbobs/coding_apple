2d4cf8be67c1b9a895d00b5c191daabe0a231a5a
JBY <qpv008@gmail.com>
2022-10-27 18:50:18 +0900

    221027

--- node/server.js
+++ node/server.js
@@ -21,11 +21,32 @@ MongoClient.connect('mongodb+srv://admin_jb:doslzja15@cluster0.e1hz11b.mongodb.n
 
 app.post('/add',function(req,res){
     console.log('add 됨')
-    db.collection('post').insertOne({이메일 : req.body.email, 텍스트 : req.body.txtarea}, function(err, result){
-        console.log('SAVE');
-    })
+    db.collection('counter').findOne({name : '게시물갯수'},function(err,result){
+
+        let totalPost = result.totalPost;
+        db.collection('post').insertOne({_id: totalPost+ 1,이메일 : req.body.email, 텍스트 : req.body.txtarea}, function(err, result){
+            console.log('SAVE');
+            db.collection('counter').updateOne({name:"게시물갯수"},{$inc : {totalPost : 1}}, function(err,result){
+                if(err){return console.log(error)};
+                
+            });
+            console.log(totalPost);
+        });
+
+
+
+    });
 });
 
+app.get('/list', function(req,res){
+
+    db.collection('post').find().toArray(function(err, result){
+        console.log(result);
+        res.render('list.ejs', {posts : result});
+    });
+
+   
+})
 
 app.get('/pet',function(req, res){
     res.send('펫 용품을 쇼핑할 수 있는 페이지입니다.')
--- study-react/src/App.js
+++ study-react/src/App.js
@@ -5,39 +5,67 @@ import { useState } from 'react';
 
 function App() {
 
-  let [post,postSet] = useState(['남자 코트 추천','강남 우동맛집','파이썬독학']);
-  let [thumb, thumbSet] = useState(0);
+  let [post, postSet] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [thumb, thumbSet] = useState([0,0,0]);
+  let [modal, modalSet] = useState(false);
+  let [count, countSet] = useState(0);
+  let [title, titleSet] = useState('');
 
-  function titleChange(){
+  function titleChange() {
     let postCopy = [...post];
-     postCopy[0] = '여자 코트 추천';
-     postSet(postCopy);
+    postCopy[0] = '여자 코트 추천';
+    postSet(postCopy);
   }
+
+  function modalPop() {
+    if ((count % 2) == 0) {
+      modalSet(true);
+      countSet(count += 1);
+    } else if ((count % 2) == 1) {
+      modalSet(false);
+      countSet(count += 1);
+    }
+  }
+
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>블로그임</h4>
       </div>
-      <div className="list">
-        <h4>{post[0]}</h4>
-        <p>2월 17일 발행 <span onClick={function(){
-          thumbSet(thumb + 1)
-        }}>❤</span> {thumb}</p>
-        <button onClick={titleChange}>버튼</button>
-      </div>
-        <Modal></Modal>
+      {
+        post.map(function (e, i) {
+          return (
+            <div className="list">
+              <h4 onClick={function(){
+                modalPop();
+                titleSet(post[i]);
+              }}>{post[i]}</h4>
+              <p>2월 17일 발행 <span onClick={function () {
+                let copy = [...thumb];
+                copy[i] = copy[i]+1;
+                thumbSet(copy);
+              }}>❤</span> {thumb[i]}</p>
+              <button onClick={titleChange}>버튼</button>
+            </div>
+          )
+        })
+      }
+      {
+        modal == true ? <Modal post={post} titleChange={titleChange} title={title}></Modal> : null
+      }
     </div>
   );
 }
 
-function Modal(){
-  return(
+function Modal(props) {
+  return (
     <div className="modal">
-    <h4>제목</h4>
-    <p>날짜</p>
-    <p>상세내용</p>
-  </div>
+      <h4>{props.title}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button onClick={props.titleChange}>글수정</button>
+    </div>
   )
 }
 
