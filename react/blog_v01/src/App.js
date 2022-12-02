94c00853869d50396ce43ea257ae74ed78727fbd
JBY <qpv008@gmail.com>
2022-12-02 18:25:59 +0900

    react map

--- react/blog_v01/src/App.js
+++ react/blog_v01/src/App.js
@@ -6,8 +6,8 @@ function App() {
 
   let post = '강남 우동 맛집';
   let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬독학'])
-  let [따봉, 따봉변경] = useState(0);
-
+  let [따봉, 따봉변경] = useState([0,0,0]);
+  let [modal, setModal] = useState(false);
   function thumbsUp(){
 
   }
@@ -33,20 +33,40 @@ function App() {
       <div className="black-nav">
         <h4>블로그임</h4>
       </div>
-      <div className="list">
-        <h4 onClick={titleSort}>{글제목[0]} <span onClick={thumbsUp}>👍</span> {따봉} </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]} <span>👍</span> {따봉} </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[2]} <span>👍</span> {따봉} </h4>
-        <p>2월 17일 발행</p>
-      </div>
+      {
+        글제목.map(function(e,i){
+          return(
+            <div className="list" key={i}>
+            <h4>{글제목[i]} <span onClick={()=>{
+              let copy = [...따봉];
+              copy[i] = copy[i] + 1;
+              따봉변경(copy);
+            }}>👍</span> {따봉[i]} </h4>
+            <p  onClick={function(){
+              setModal(!modal);
+            }}>2월 17일 발행</p>
+          </div>
+          )
+
+        })
+      } 
+      {
+        modal == true ? <Modal></Modal> : null
+        
+      }
+      
     </div>
   );
 }
 
+
+function Modal(){
+  return(
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
 export default App;
